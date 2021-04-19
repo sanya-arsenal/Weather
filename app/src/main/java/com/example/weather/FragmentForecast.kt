578935cafddedea97c4fc9ef6964ddfa3e114213
Alexandr <sanya-arsenal@mail.ru>
2021-04-19 12:11:58 +0300

    Add fragments, layouts, icons

---
+++ app/src/main/java/com/example/weather/FragmentForecast.kt
@@ -0,0 +1,5 @@
+package com.example.weather
+
+import androidx.fragment.app.Fragment
+
+class FragmentForecast: Fragment(R.layout.fragment_weather_forecast)
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val first = FragmentToday()
+        openFragment(first)
     }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.page_today -> {
+                val first = FragmentToday()
+                openFragment(first)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.page_forecast -> {
+                val second = FragmentForecast()
+                openFragment(second)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment:Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_content,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
